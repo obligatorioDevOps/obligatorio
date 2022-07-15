026a2fe9fbdf87a592fc76cffab837abc3deb1cd
Alejandro Bragança <alejandro.braganca@gmail.com>
2022-07-14 21:19:54 -0300

    terraform fixes

--- iam.tf
+++ iam.tf
@@ -22,11 +22,7 @@ data "aws_iam_policy" "ssm_managed_instance" {
   arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
- resource "aws_iam_role_policy_attachment" "karpenter_ssm_policy" {
-  role       = module.eks.worker_iam_role_name
-  policy_arn = data.aws_iam_policy.ssm_managed_instance.arn
-} 
-
+ 
 
 
 
