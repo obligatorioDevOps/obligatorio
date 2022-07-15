b74f6f7ce6ba68aa06df148dcf3b8e38a6f7c686
Alejandro Bragança <alejandro.braganca@gmail.com>
2022-07-14 22:10:07 -0300

    alb and helm

--- helm.tf
+++ helm.tf
@@ -12,35 +12,7 @@ provider "helm" {
   }
 }
 
-resource "helm_release" "karpenter" {
-  namespace        = "karpenter"
-  create_namespace = true
 
-  name       = "karpenter"
-  repository = "https://charts.karpenter.sh"
-  chart      = "karpenter"
-  version    = "v0.11.0"
-
-  set {
-    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = module.karpenter_irsa.iam_role_arn
-  }
-
-  set {
-    name  = "clusterName"
-    value = module.eks.cluster_id
-  }
-
-  set {
-    name  = "clusterEndpoint"
-    value = module.eks.cluster_endpoint
-  }
-
-  set {
-    name  = "aws.defaultInstanceProfile"
-    value = aws_iam_instance_profile.karpenter.name
-  }
-}
 
 resource "helm_release" "aws-load-balancer-controller" {
   namespace        = "kube-system"
