c375da9a1a3a5343f55145fe8f795f6dd2217684
Alejandro Bragança <alejandro.braganca@gmail.com>
2022-07-18 00:53:48 -0300

    remove main profile

--- route53.tf
+++ route53.tf
@@ -9,3 +9,59 @@ resource "aws_route53_zone" "private" {
     vpc_id = module.vpc.vpc_id
   }
 }
+
+resource "aws_route53_record" "products" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = "products"
+  type    = "A"
+ 
+  
+  alias {
+    name                   = aws_lb.obligatorio.dns_name
+    zone_id                = aws_lb.obligatorio.zone_id
+    evaluate_target_health = false
+  }
+  
+}
+
+resource "aws_route53_record" "orders" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = "orders"
+  type    = "A"
+ 
+  
+  alias {
+    name                   = aws_lb.obligatorio.dns_name
+    zone_id                = aws_lb.obligatorio.zone_id
+    evaluate_target_health = false
+  }
+  
+}
+
+resource "aws_route53_record" "payments" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = "payments"
+  type    = "A"
+ 
+  
+  alias {
+    name                   = aws_lb.obligatorio.dns_name
+    zone_id                = aws_lb.obligatorio.zone_id
+    evaluate_target_health = false
+  }
+  
+}
+
+resource "aws_route53_record" "shipping" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = "shipping"
+  type    = "A"
+ 
+  
+  alias {
+    name                   = aws_lb.obligatorio.dns_name
+    zone_id                = aws_lb.obligatorio.zone_id
+    evaluate_target_health = false
+  }
+  
+}
